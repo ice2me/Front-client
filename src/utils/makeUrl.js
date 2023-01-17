e6058b273c009426d853487c9a2719d01faa79b0
ice2me <ice2me1989@gmail.com>
2023-01-17 14:14:57 +0200

    2

--- src/utils/makeUrl.js
+++ src/utils/makeUrl.js
@@ -1,6 +1,6 @@
 export const apiBaseUrl =
 	process.env.NODE_ENV === "development"
-		? "http://localhost:5000/api/"
+		? "http://localhost:8080/api/"
 		: `${window.location.origin}/api/`;
 
 export const makeUrl = (urlSuff) => `${apiBaseUrl}/${urlSuff}`;
