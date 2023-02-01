e9ba715e9720176c830c1f6347554b4367bd5ad2
ice2me <ice2me1989@gmail.com>
2023-02-01 18:33:18 +0200

    fix stile profile

--- src/views/ContactSupport/ContactSupport.js
+++ src/views/ContactSupport/ContactSupport.js
@@ -13,6 +13,8 @@ const ContactSupport = ({
 	shop
 }) => {
 	const contact = shop.socials_links
+	console.log(shop.email)
+	console.log(shop.phone)
 	return (
 		<>
 			<div className='profile'>
@@ -28,83 +30,90 @@ const ContactSupport = ({
 						/>
 					</button>
 				</h1>
-				<div className=" mt-5 d-flex align-items-center flex-wrap justify-content-center h-25 profile-body_content-socials">
-					<h2>
+				<div className=" mt-5 d-flex align-items-start flex-column justify-content-center h-25 profile-body_content-socials">
+					<h2 className='d-flex flex-column'>
 						<FormattedMessage id='shopContact' />
 					</h2>
-					{
-						contact?.shop_facebook &&
-						<a href={contact?.shop_facebook}>
-							<img
-								src={facebook}
-								alt="facebook"
-							/>
-						</a>
-					}
+					<a href={`mailto:${shop.email}`}>
+						<FormattedMessage id='email' />
+						{shop.email}</a>
+					<a href={`tel:${shop.phone}`}>
+						<FormattedMessage id='phone' />
+						{shop.phone}</a>
+					<div>
+						{
+							contact?.shop_facebook &&
+							<a href={contact?.shop_facebook}>
+								<img
+									src={facebook}
+									alt="facebook"
+								/>
+							</a>
+						}
+						{
+							contact?.shop_viber &&
+							<a href={`viber://add?number=%${contact?.shop_viber}`}>
+								<img
+									src={viber}
+									alt="viber"
+								/>
+							</a>
+						}
+						{
+							contact?.shop_telegram &&
+							<a href={`tg://resolve?domain=${contact?.shop_telegram}`}>
+								<img
+									src={telegram}
+									alt="telegram"
+								/>
+							</a>
+						}
+						{
+							contact?.shop_instagram &&
+							<a href={`https://www.instagram.com/${contact?.shop_instagram}/`}>
+								<img
+									src={instagram}
+									alt="instagram"
+								/>
+							</a>
+						}
+					</div>
+					<h2 className=' text-center mt-5'>
+						<FormattedMessage id='shopContactText' />
+					</h2>
+				</div>
 
-					{
-						contact?.shop_viber &&
-						<a href={`viber://add?number=%${contact?.shop_viber}`}>
+				<div className="d-flex align-items-start flex-column justify-content-center h-20 profile-body_content-socials">
+					<h2>
+						<FormattedMessage id='developerContact' />
+					</h2>
+					<div>
+						<a href={`viber://add?number=%380669696402`}>
 							<img
 								src={viber}
 								alt="viber"
 							/>
 						</a>
-					}
-
-					{
-						contact?.shop_telegram &&
-						<a href={`tg://resolve?domain=${contact?.shop_telegram}`}>
+						<a href={`tg://resolve?domain=@ice2me`}>
 							<img
 								src={telegram}
 								alt="telegram"
 							/>
 						</a>
-					}
-
-					{
-						contact?.shop_instagram &&
-						<a href={`https://www.instagram.com/${contact?.shop_instagram}/`}>
+						<a
+							href={`https://www.instagram.com/serhiizuiev/`}
+							target='_blank'
+						>
 							<img
 								src={instagram}
 								alt="instagram"
 							/>
 						</a>
-					}
-					<h2 className=' text-center mt-5'>
-						<FormattedMessage id='shopContactText' />
-					</h2>
-				</div>
-				<div className="d-flex align-items-center flex-wrap justify-content-center h-20 profile-body_content-socials">
-					<h2>
-						<FormattedMessage id='developerContact' />
-					</h2>
-					<a href={`viber://add?number=%380669696402`}>
-						<img
-							src={viber}
-							alt="viber"
-						/>
-					</a>
-					<a href={`tg://resolve?domain=@ice2me`}>
-						<img
-							src={telegram}
-							alt="telegram"
-						/>
-					</a>
-					<a
-						href={`https://www.instagram.com/serhiizuiev/`}
-						target='_blank'
-					>
-						<img
-							src={instagram}
-							alt="instagram"
-						/>
-					</a>
+					</div>
 					<h2 className=' text-center mt-5'>
 						<FormattedMessage id='developerContactText' />
 					</h2>
 				</div>
-
 			</div>
 			<div
 				onClick={toggleContactSupport}
